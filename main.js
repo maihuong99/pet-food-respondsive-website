a02a216781408cc54c11190f5d038f1c9100257d
maihuong99 <62054925+maihuong99@users.noreply.github.com>
2021-07-28 18:32:23 +0700

    edit the stars's color

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
- 
+    console.log($('html').css("font-size"));
     $('#search-icon').click(function(){
         $(this).toggleClass('fa-times');
         $('#search-box').toggleClass('active');
